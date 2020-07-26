167b8c78dc87b86051fc335c4f14774f41f80a16
daybreaksnow <daybreaksnow@gmail.com>
2020-07-26 22:55:57 +0900

    船長のオリキャラSTGを追加

---
+++ ahoy-stg/resize.js
@@ -0,0 +1,7 @@
+function resize(canvas) {
+    var canvas = document.getElementById('#canvas');
+    canvas.width = 1024;
+    canvas.height = 768;
+    canvas.clientWidth = 1024;
+    canvas.clientHeight = 768;
+}
